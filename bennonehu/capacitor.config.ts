2af51f3e3f06fe3b6c0a8bf8dc28cc2ea6c26e33
bennone1 <75794362+bennone1@users.noreply.github.com>
2022-03-06 21:51:04 +0100

    Add files via upload

---
+++ bennonehu/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'hu.bennone',
+  appName: 'Bennone App',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
